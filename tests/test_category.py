f6ba448c05e8be3825d99bd847f475597ee1fe4c
Olga Python <olga_python@proton.me>
2025-04-28 20:08:26 +0200

    update tests and conftest.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,9 +1,18 @@
 from src.category import Category
 
 
-def test_category_init(first_category, first_product, second_product):  # передаём fixtures
+def test_category_init(first_category, first_product, second_product):  # передаём fixtures из conftest.py
     assert first_category.name == "Смартфоны"
     assert first_category.description == "Смартфоны, как средство не только коммуникации, но и дополнительных функций"
-    assert first_category.products == [first_product, second_product]
+    assert "Samsung" in first_category.products
+    assert "Iphone" in first_category.products
     assert Category.category_count == 1
     assert Category.product_count == 2
+
+
+def test_add_product(first_category, forth_product):  # передаём fixtures из conftest.py
+    assert Category.product_count == 2
+    first_category.add_product(forth_product)
+
+    assert "Xiaomi" in first_category.products
+    assert Category.product_count == 3

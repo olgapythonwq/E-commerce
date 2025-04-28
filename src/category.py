6d6a03d92fc0c30ba0684ba36ce096ebd2d62da2
Olga Python <olga_python@proton.me>
2025-04-28 20:08:02 +0200

    update category.py and product.py

--- src/category.py
+++ src/category.py
@@ -1,21 +1,37 @@
-from typing import Optional, List
+from typing import List, Optional
 
 from src.product import Product
 
 
 class Category:
-    """Класс для описания категорий"""
+    """Класс для описания категорий товаров"""
+    # Атрибуты экземпляра:
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
-    category_count = 0
-    product_count = 0
+    # Атрибуты класса:
+    category_count: int = 0
+    product_count: int = 0
 
     def __init__(self, name: str, description: str, products: Optional[List[Product]] = None) -> None:
+        """Инициализация категории"""
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product: Product) -> None:
+        """Функция, осуществляющая добавление продукта в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Функция с методом геттера для получения списка продуктов"""
+        products_list = ""
+        for product in self.__products:
+            products_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_list
--- src/product.py
+++ src/product.py
@@ -1,16 +1,72 @@
+from typing import Any
+
+
 class Product:
     """Класс для описания продуктов"""
+    # Атрибуты экземпляра:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
+    # Атрибуты класса:
+    products: list = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        """Инициализация экземпляра продукта."""
         self.name = name
         self.description = description
+
         if price <= 0:
             raise ValueError("Цена не может быть отрицательной или равной нулю")
-        self.price = price
+        self.__price = price
+
         if quantity <= 0:
             raise ValueError("Количество не может быть отрицательным или равным нулю")
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_dict: dict) -> Any:
+        """Класс-метод создающий новый продукт на основе словаря"""
+        # Проверяем и приводим типы данных в словаре перед использованием
+        name = str(product_dict.get("name", ""))  # второй аргумент .get() — это значение по умолчанию
+        description = str(product_dict.get("description", ""))  # Если ключа "name" нет, то вернется пустая строка ""
+        price_raw = product_dict.get("price")
+        quantity_raw = product_dict.get("quantity")
+
+        if not isinstance(price_raw, (int, float)):
+            raise ValueError("Цена должна быть числом")
+        if not isinstance(quantity_raw, int):
+            raise ValueError("Количество должно быть целым числом")
+
+        price = float(price_raw)
+        quantity = int(quantity_raw)
+
+        for product in cls.products:  # Проверка есть ли продукт в списке продуктов
+            if product.name == name:  # Если продукт существует, то обновим количество и цену
+                product.quantity += quantity
+                if product.price < price:  # Если цена сущ-го продукта меньше, то обновим цену
+                    product.price = price
+                return product
+        # Если продукта не существует, то создадим его
+        new_product = cls(name, description, price, quantity)
+        cls.products.append(new_product)
+        return new_product
+
+    @property
+    def price(self) -> float:
+        """Функция с методом геттер, возвращающая приватный атрибут цена"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Функция с методом сеттер, для установки новой цены"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        if new_price < self.__price:
+            print(f"Вы уверены, что хотите снизить цену с {self.__price} до {new_price}?")
+            user_answer = input("Введите Y для подтверждения или N для сохранения текущей цены. (Y/N): ")
+            if user_answer.lower() == "y":
+                self.__price = new_price
+        else:
+            self.__price = new_price
